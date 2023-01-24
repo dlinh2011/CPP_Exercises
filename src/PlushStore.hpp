7ffaf71feb9671b2ee949deb0fb6de268c207fbd
PHAM Linh <dieu-linh.pham@edu.univ-eiffel.fr>
2023-01-24 11:40:49 +0100

    test-07 pass

--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,20 @@
 #pragma once
+#include <string>
+
+class PlushStore {
+    public:
+        PlushStore (std::string name) : _name {name} {}
+        std::string get_name() const {
+            return _name;
+        }
+        int get_wealth_amount() const {
+            return _wealth; 
+        }
+        int get_stock_size() const {
+            return _stock;
+        }
+    private: 
+        std::string _name;
+        int _wealth = 0;
+        int _stock = 0; 
+};
