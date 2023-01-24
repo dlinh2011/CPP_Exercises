9ddf9a4d8de88d9ac5eab36cd1fa09b039295360
PHAM Linh <dieu-linh.pham@edu.univ-eiffel.fr>
2023-01-24 12:08:53 +0100

    test-11 pass

--- src/Kid.hpp
+++ src/Kid.hpp
@@ -1,1 +1,17 @@
 #pragma once
+#include <string>
+
+class Kid {
+    public:
+        Kid (std::string name, int money) : _name { name }, _money { money } {}; 
+        std::string get_name() const {
+            return _name;
+        }
+        int get_money() const {
+            return _money; 
+        }
+    private:
+        std::string _name; 
+        int _money; 
+
+};
