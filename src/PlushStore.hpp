b5a2dd937e4dadd57b7cabe963150d72c31f6848
PHAM Linh <dieu-linh.pham@edu.univ-eiffel.fr>
2023-01-24 12:02:12 +0100

    test-10 pass

--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <algorithm> 
 
 class PlushStore {
     public:
@@ -20,19 +21,26 @@ class PlushStore {
         int get_debt_amount() const {
             return _debt;
         }
-        void make_plush(int cost) {
-            if (_wealth == 0) return;
-            if (cost > _wealth) {
+        int get_experience() const {
+            return _exp;
+        }
+        int make_plush(int cost) {
+            if (_wealth == 0) return 0;
+            if (cost > _wealth) 
                 _wealth = 0; 
-            }
-            else {
+            else 
                 _wealth -= cost;
-            }
             _stock++;
+            _exp++;
+            return value_plush(cost);
         }
     private: 
         std::string _name;
         int _wealth = 0;
         int _stock = 0;
         int _debt = 0; 
+        int _exp = 0;
+        int value_plush(int value_invest) const {
+            return value_invest + std::max(_exp, _exp * value_invest / 100);
+        }
 };
