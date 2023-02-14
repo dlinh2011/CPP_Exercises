fac23a2f2f69fcaf861f7922f97052e0b980c579
PHAM Linh <dieu-linh.pham@edu.univ-eiffel.fr>
2023-02-14 09:31:52 +0100

    test 03 ok

--- src/Card.hpp
+++ src/Card.hpp
@@ -1,1 +1,52 @@
 #pragma once
+#include <string>
+enum CardType {
+    Monster, Spell, Trap
+};
+
+class Card {
+    public:
+        Card(const std::string& id, CardType type) : _id {id} , _type {type} {} 
+        const std::string& get_id() const {
+            return _id; 
+        }
+        CardType get_type() const {
+            return _type; 
+        }
+        const std::string& get_name() const {
+            return _name;
+        }
+        void set_name (const std::string& name) {
+            _name = name;
+        }
+        const std::string& get_description() const {
+            return _description;
+        }
+        void set_description (const std::string& description) {
+            _description = description;
+        }
+
+
+    private:
+        std::string _id;
+        std::string _name; 
+        std::string _description;
+        CardType _type;
+};
+
+
+std::string to_string(CardType type) {
+    switch (type) {
+        case CardType::Monster:
+            return "Monster";
+            break;
+        case CardType::Spell:
+            return "Spell";
+            break;
+        case CardType::Trap:
+            return "Trap";
+            break;
+        default:
+            return "";
+    }
+} 
--- src/Monster.hpp
+++ src/Monster.hpp
@@ -1,1 +1,37 @@
 #pragma once
+#include <string>
+
+enum class Attribute {
+   Dark, Divine, Earth, Fire, Light, Water, Wind
+};
+
+
+
+const std::string to_symbol(Attribute attribute) {
+    switch (attribute) {
+        case Attribute::Dark:
+            return u8"闇";
+            break;
+        case Attribute::Divine:
+            return u8"神";
+            break;
+        case Attribute::Earth:
+            return u8"地";
+            break;
+        case Attribute::Fire:
+            return u8"炎";
+            break;
+        case Attribute::Light:
+            return u8"光";
+            break;
+        case Attribute::Water:
+            return u8"水";
+            break;
+        case Attribute::Wind:
+            return u8"風";
+            break;
+
+        default:
+            return "";
+    }
+} 
