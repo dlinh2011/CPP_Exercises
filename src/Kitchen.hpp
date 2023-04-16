c7fb0065d1a032f70d7a8830b96c1eb6e03bc006
DIEU-LINH PHAM <dieu-linh.pham@edu.univ-eiffel.fr>
2023-04-16 17:28:15 +0200

    problem avec le Set

--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,45 @@
 #pragma once
+#include "../lib/Unit.hpp"
+
+#include <algorithm>
+#include <cctype>
+#include <list>
+#include <memory>
+#include <string>
+#include <vector>
+
+using UnitPtr = std::unique_ptr<Unit>;
 
 class Kitchen
-{};
+{
+public:
+    Unit& register_unit(Unit unit)
+    {
+        auto ptr = std::make_unique<Unit>(unit);
+        _units.push_back(std::move(ptr));
+        return *(_units.back());
+    }
+
+    Ingredient& register_ingredient(Ingredient ingredient) { return _ingredients.emplace_back(ingredient); }
+
+    Ingredient* find_ingredient(std::string& name)
+    {
+        auto result =
+            std::find_if(begin(_units), end(_units), [&name](UnitPtr& ptr) { return name == ptr->name; });
+    }
+
+    Unit* find_unit(const std::string& name)
+    {
+        auto result =
+            std::find_if(begin(_units), end(_units), [&name](UnitPtr& ptr) { return name == ptr->name; });
+        if (result == _units.end())
+        {
+            return nullptr;
+        }
+        return &(*(*result));
+    }
+
+private:
+    struct Compare
+    {};
+};
